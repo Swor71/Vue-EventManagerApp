a2eabd3ff29dacb8c35d058105c46d71385f00c5
Marcin Swornowski <swor71@gmail.com>
2018-10-02 16:20:39 +0200

    Add add event component

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
-    <h3>Dashboard</h3>
-    <button class="btn btn-danger btn-sm" @click="signOut">Sign Out</button>
+    <div class="d-flex justify-content-between">
+      <h3 class="">Dashboard</h3>
+      <button class="btn btn-danger" @click="signOut">Sign Out</button>
+    </div>
+    <hr>
+      <AddEvent />
+    <hr>
   </div>
 </template>
 
 <script>
 import { firebaseApp } from '../firebase';
+import AddEvent from './AddEvent';
+
 export default {
   methods: {
     signOut() {
       this.$store.dispatch('signOut');
       firebaseApp.auth().signOut();
     }
+  },
+  components: {
+    AddEvent
   }
 };
 </script>
